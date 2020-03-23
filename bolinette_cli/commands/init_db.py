0d515f0ad5778cb473b1544967c743b92c554060
Pierre Chat <pierrechat89@hotmail.fr>
2020-03-23 23:17:26 +0100

    Upgraded to new Bolinette version

--- bolinette_cli/commands/init_db.py
+++ bolinette_cli/commands/init_db.py
@@ -1,12 +1,2 @@
-from bolinette import transaction, db
-from bolinette.database import run_seeders
-
-
-def init_db(parser, **options):
-    seed = options.get('seed', False)
-    with parser.blnt.app.app_context():
-        db.drop_all()
-        db.create_all()
-        if seed:
-            with transaction:
-                run_seeders()
+def init_db(parser, seed, **_):
+    parser.blnt.run_command('init_db', run_seeders=seed)
--- bolinette_cli/commands/run_server.py
+++ bolinette_cli/commands/run_server.py
@@ -1,6 +1,2 @@
-import sys
-
-
 def run_server(parser):
-    from bolinette import env
-    parser.blnt.app.run(host=env['FLASK_HOST'], port=env['FLASK_PORT'])
+    parser.blnt.run_command('run_server')
